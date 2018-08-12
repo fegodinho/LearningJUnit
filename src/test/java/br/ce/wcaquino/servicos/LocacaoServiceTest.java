74db5d83e5f47c17190e5fcaae1cc4fd8364aa6c
Felipe Godinho <fegodinho@gmail.com>
2018-08-12 20:57:34 -0300

    Aula 7 - AssertThat

--- src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -1,16 +1,22 @@
 package br.ce.wcaquino.servicos;
 
 
+import static br.ce.wcaquino.utils.DataUtils.isMesmaData;
+import static br.ce.wcaquino.utils.DataUtils.obterDataComDiferencaDias;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
 import java.util.Date;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
-import br.ce.wcaquino.servicos.LocacaoService;
-import br.ce.wcaquino.utils.DataUtils;
 
 public class LocacaoServiceTest {
 	
@@ -26,9 +32,15 @@ public class LocacaoServiceTest {
 		Locacao locacao =  service.alugarFilme(usuario, filme);
 		
 		//verificacao
-		Assert.assertEquals(5.0,locacao.getValor(), 0.01);
-		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
-		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+		assertEquals(5.0,locacao.getValor(), 0.01);
+		assertThat(locacao.getValor(),is(5.0));
+		assertThat(locacao.getValor(),is(equalTo(5.0)));
+		assertThat(locacao.getValor(),is(not(6.0)));
+		
+		assertTrue(isMesmaData(locacao.getDataLocacao(), new Date())); //ou
+		assertThat(isMesmaData(locacao.getDataLocacao(), new Date()),is(true));
+		assertTrue(isMesmaData(locacao.getDataRetorno(), obterDataComDiferencaDias(1))); //ou
+		assertThat(isMesmaData(locacao.getDataRetorno(), obterDataComDiferencaDias(1)), is(true));
 				
 	}
 
