63ad18db33b205d38db292319671eef79ee772a6
Felipe Godinho <fegodinho@gmail.com>
2018-10-30 18:58:55 -0300

    Aula 34 - Spy

--- src/test/java/br/ce/wcaquino/servicos/CalculadoraMockTest.java
+++ src/test/java/br/ce/wcaquino/servicos/CalculadoraMockTest.java
@@ -1,11 +1,47 @@
 package br.ce.wcaquino.servicos;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 
 public class CalculadoraMockTest {
+	
+	@Mock
+	private Calculadora calcMock;
+	
+	@Spy
+	private Calculadora calcSpy;
+	
+	@Mock
+	private EmailService email; 
+	
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+	}
+	@Test
+	public void devoMostrarDiferencaEntreMockSpy() {
+		Mockito.when(calcMock.somar(1, 2)).thenReturn(5);
+		//Mockito.when(calcSpy.somar(1, 2)).thenReturn(5);
+		Mockito.doReturn(5).when(calcSpy).somar(1, 2);
+		Mockito.doNothing().when(calcSpy).imprime();
+		
+		System.out.println("Mock "+calcMock.somar(1, 2));
+		System.out.println("Spy "+calcSpy.somar(1, 2));
+		
+		System.out.println("Mock");
+		calcMock.imprime();
+		System.out.println("Spy");
+		calcSpy.imprime();	
+		
+	}
+	
+	
 	
 	@Test
 	public void teste() {
